1e7ad98ee790079c9c1ec399e8f1baea9c4a48cc
Andres Lemos <Lemos_1325@hotmail.com>
2020-09-29 18:17:54 -0500

    Se crea el controlador para la pagina de inicio

--- src/main/java/com/empleos/controller/HomeController.java
+++ src/main/java/com/empleos/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.empleos.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String ShowHome() {
+	public String ShowHome(Model model) {
+		/*
+		model.addAttribute("mensaje", "Bienvenidos a empleos App");
+		model.addAttribute("fecha", new Date());
+		*/
+		
+		String nombre = "Auxiliar de Contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000.0;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fecha", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+			
 		return "index";
 	}
 
